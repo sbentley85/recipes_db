8086c4ef320a0c21e34aec3836caff659e2596fd
sbentley85 <61815607+sbentley85@users.noreply.github.com>
2020-07-08 11:20:49 +1200

    set up basic react app with express

--- server/server.js
+++ server/server.js
@@ -3,13 +3,23 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
+// for production use build folder for static files
+
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
-
+// serve the client index.html file for all requests
 app.get('*', (req, res) => {
-    res.sendFile(path.join(buildPath, 'index.html'));
- });
+res.sendFile(path.join(buildPath, 'index.html'));
+});
 
+/*
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+// serve the client index.html file for all requests
+app.get('*', (req, res) => {
+res.sendFile(path.join(publicPath, 'index.html'));
+});
+*/
 
 app.listen(port, () => {
     console.log(__dirname);
