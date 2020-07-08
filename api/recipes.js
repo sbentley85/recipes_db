cc42223b5ec3843fa2a7830714fc13438ad8c6a3
sbentley85 <61815607+sbentley85@users.noreply.github.com>
2020-07-08 13:46:43 +1200

    added db query to recipes route

--- api/recipes.js
+++ api/recipes.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const recipesRouter = express.Router();
 
-recipesRouter.get('/', (req, res, next) => {
-    console.log('getting recipes');
-    res.sendStatus(200);
+const Pool = require('pg').Pool
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+
+
+recipesRouter.get('/', (req,res,next) => {
+    console.log('getting recipes')
+    db.query('SELECT * FROM recipe ORDER BY id ASC', (err, recipes) => {
+        if (err) {
+            
+            next(err);
+        } else {
+            
+            res.status(200).json({recipes: recipes})
+        }
+    })
 })
 
 module.exports = recipesRouter;
