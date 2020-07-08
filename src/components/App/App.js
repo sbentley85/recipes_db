f6becff2beced8acd9d2c0e7886db0451aff563f
sbentley85 <61815607+sbentley85@users.noreply.github.com>
2020-07-08 16:55:55 +1200

    adds navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,14 @@ import Container from 'react-bootstrap/Container';
 import './App.css';
 import { Auth0Context } from '../../contexts/auth0-context';
 import PrivateRoute from '../auth/PrivateRoute';
+import NavBar from '../NavBar/NavBar';
 
 function App() {
   return (
     <Router>
         <Container fluid>
           
-          
+        <NavBar />  
           
           
           
