ac47a54724fd80d4a92ba7f42f0a3584d56fe6e4
sbentley85 <61815607+sbentley85@users.noreply.github.com>
2020-07-13 17:30:34 +1200

    adds links to tags

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Tags = (props) => {
     
     if(props.tags) {
     return (
-        props.tags.map(tag => <span className="tag">{tag.tag}</span>)
+        props.tags.map(tag => <Link to={`/tags/${tag.tag}`}><span className="tag">{tag.tag}</span></Link>)
     )
     } else {
         return null
